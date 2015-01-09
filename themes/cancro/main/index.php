5eb880f94f79829e50548c5686ab911b5bd391a9
lanc33 <lancee@yandex.com>
2015-01-09 10:07:45 -0200

    Adiciona o tema cancro como cópia do default.

---
+++ themes/cancro/main/index.php
@@ -0,0 +1,3 @@
+<?php if (!defined('FLUX_ROOT')) exit; ?>
+<h2><?php echo htmlspecialchars(Flux::message('MainPageHeading')) ?></h2>
+<p style="text-align: right"><strong><em><?php echo htmlspecialchars(Flux::message('MainPageThanks')) ?></em></strong></p>
