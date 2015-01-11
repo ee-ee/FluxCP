c2bf2cd4afe68a4d87476119f49e6f032238e5dc
lanc33 <lancee@yandex.com>
2015-01-11 19:33:52 -0200

    Atualiza link do patcher

--- themes/cancro/main/index.php
+++ themes/cancro/main/index.php
@@ -7,7 +7,7 @@
 <a href="http://www.levelupgames.com.br/redirecionador/ragnarok/arquivo/cliente-do-jogo">Download direto</a><br>
 <a href="http://www.levelupgames.com.br/redirecionador/ragnarok/arquivo/torrent-cliente-do-jogo">Torrent</a></li>
 <li>Após instalar o bRO, aplique nosso patch:<br>
-<a href="#">Download direto</a></li>
+<a href="http://patcher.vhschan.org/patcher-cancRO.exe">Download direto</a></li>
 <li><a href="/?module=account&action=create">Registre-se</a> no menu ao lado e bom jogo!</li>
 </ol>
 <p style="text-align: right"><strong><em>O dobro da diversão pela metade do preço!</em></strong></p>
