4796bc3ca2a131e0d2b7905f1ecff37f7df05fea
lanc33 <lancee@yandex.com>
2015-01-11 16:15:37 -0200

    Instruções na home.

--- themes/cancro/main/index.php
+++ themes/cancro/main/index.php
@@ -1,3 +1,13 @@
 <?php if (!defined('FLUX_ROOT')) exit; ?>
 <h2>Bem vindo ao cancRO!</h2>
+<p>Nossas rates: 50/50/30.</p>
+<h3>Instruções para jogar</h3>
+<ol>
+<li>Instale o cliente do bRO:<br>
+<a href="http://www.levelupgames.com.br/redirecionador/ragnarok/arquivo/cliente-do-jogo">Download direto</a><br>
+<a href="http://www.levelupgames.com.br/redirecionador/ragnarok/arquivo/torrent-cliente-do-jogo">Torrent</a></li>
+<li>Após instalar o bRO, aplique nosso patch:<br>
+<a href="#">Download direto</a></li>
+<li><a href="/?module=account&action=create">Registre-se</a> no menu ao lado e bom jogo!</li>
+</ol>
 <p style="text-align: right"><strong><em>O dobro da diversão pela metade do preço!</em></strong></p>
